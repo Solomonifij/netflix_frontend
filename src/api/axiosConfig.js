a9a157f25c41cb1ffd1cff3ff680006e8c7eaa7d
solomon-cloud <solomon@gmail.com>
2026-04-02 15:09:20 +0100

    Added frontend file

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://{your-backend-url}/api',
+    baseURL:'http://3.239.105.80:8080',
     headers: {
         'Content-Type': 'application/json',
     },
