a0d81a70aae29638f2dd6cb033e52a13bdf9c28a
solomon-cloud <solomon@gmail.com>
2026-04-14 08:50:03 +0100

    updated

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://3.15.194.21:8080',
+    baseURL:'http://3.144.214.85:8080',
     headers: {
         'Content-Type': 'application/json',
     },
